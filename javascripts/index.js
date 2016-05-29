7c7d63001193592572fa1eb14e49a08f4d3c2696
kumaheika <w87502047@gmail.com>
2016-05-29 21:39:38 +0800

    myProject

---
+++ javascripts/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  //圖片 lazyload
+  $("img.lazy").lazyload({
+    effect: "fadeIn"
+  });
+  //獲取現在螢幕上方
+  $(window).scroll(function () {
+    var scrollVal = $(this).scrollTop();
+    console.log(scrollVal);
+  });
+
+  var $tab01 = $('div.tab01').offset().top;
+  $('#gotab01').click(function() {
+    var $body = $('body');
+    $body.animate({
+      scrollTop: $('div.tab01').offset().top
+    }, 500, "swing");
+    return false;
+  });
+
+});
